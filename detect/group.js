ea283236c8bd3ee167c05117668acc34c41f30f9
BotWhatsapp12 <91938327+BotWhatsapp12@users.noreply.github.com>
2022-01-12 16:53:14 +0700

    Add files via upload

--- detect/group.js
+++ detect/group.js
@@ -5,8 +5,9 @@ const fs = require("fs-extra")
 
 const { getBuffer } = require('../lib/functions')
 const { color, bgcolor } = require('../lib/warna')
-join = '\`\`\`New Member\`\`\` \n \`\`\`Nama :\`\`\` \n \`\`\`Askot : \`\`\` \n \`\`\`Umur :\`\`\` \n \`\`\`Status :\`\`\` \n\n - [ Welcome👋 ] -'
-leave = '_*Sayonara....👋*_'
+var p = '```'
+join = '${p}Hai Selamat Datang${p} @${num.split('@')[0]}\n${p}Nama :${p}\n${p}Askot :${p}\n${p}Umur :${p}\n${p}Status :${p}\n\n - [ Welcome Ngab] -'
+leave = '_*Byeee @${num.split('@')[0]}*_'
 
 teks = `${join}`
 let setting = JSON.parse(fs.readFileSync('./setting.json'))
