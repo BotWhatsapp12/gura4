36e8ebb45a02b01a032cfe351e704ff79da1d7f6
BotWhatsapp12 <91938327+BotWhatsapp12@users.noreply.github.com>
2022-01-13 06:14:13 +0700

    Add files via upload

--- gura.js
+++ gura.js
@@ -761,14 +761,6 @@ if(budy.includes('assalamualaikum')){
 cpref = `*Waalaikumsalam Wr Wb*`
 reply(cpref)
 }
-if (isCmd && !isGroup) {
-console.log(color('「 SPAM 」','red'), (command), ">", (sender.split('@')[0]))
-return reply('```JANGAN SPAM, MOHON DI BERI JEDA !!!```')
-}        
-if (isCmd && isGroup) {
-console.log(color('「 SPAM 」','red'), (command), ">", (sender.split('@')[0]), "=>", color(groupName, "yellow"))
-return reply('```JANGAN SPAM, MOHON DI BERI JEDA !!!```')
-}  
 
 switch(command) {
 
