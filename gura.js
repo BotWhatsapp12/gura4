7decdbd3570f9acbe7d91c688aae861102e02ffc
BotWhatsapp12 <91938327+BotWhatsapp12@users.noreply.github.com>
2022-01-18 17:08:39 +0700

    Add files via upload

--- gura.js
+++ gura.js
@@ -993,7 +993,7 @@ case 'allmenu':
 if (modelmenu == "text") {
                     reply(menu)
                     } else if (modelmenu == "butpdf") {
-                    sendButpdf (from, menu, "Created by GuraBotz", tamnel, menubutlist, ftoko)
+                    sendButpdf (from, menu, "Created by GuraBotz", tamnel, menubutlist, (quoted: ftoko))
                     }
                   break
  case 'tes':
